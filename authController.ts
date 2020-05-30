0d50595eb070ce988123873758fff6b687fc410d
Saul Maldonado <samaldonado4@gmail.com>
2020-05-30 03:09:40 -0500

    initial commit

---
+++ authController.ts
@@ -0,0 +1,44 @@
+import { HttpResponse } from '@angular/common/http';
+import db from './db.json';
+/*
+
+configure 'tsconfig.ts' include "resolveJsonModule" and "esModuleInterop"
+
+tsconfig.json 
+
+  "compilerOptions": {
+    ...
+    "resolveJsonModule": true,
+    "esModuleInterop": true,
+    ...
+  }
+    
+*/
+
+export const authController = (request): HttpResponse<any> => {
+  /* ENDPOINT = '/api/authenticate' METHOD = 'POST' */
+  if (request.url === '/api/authenticate' && request.method === 'POST') {
+    const {
+      email,
+      password,
+    }: { email: string; password: string } = request.body;
+
+    let mockDbQuery = db.find(({ email: dbEmail }) => email === dbEmail);
+
+    if (mockDbQuery) {
+      if (mockDbQuery.password === password) {
+        return new HttpResponse({
+          status: 200,
+        });
+      } else {
+        return new HttpResponse({
+          status: 401,
+        });
+      }
+    } else {
+      return new HttpResponse({
+        status: 404,
+      });
+    }
+  }
+};
---
+++ mock-backend.service.ts
@@ -0,0 +1,26 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+} from '@angular/common/http';
+import { of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import { authController } from './authController';
+
+export class fakeBackend implements HttpInterceptor {
+  /* simulated delay value in ms */
+  delay = 2000;
+
+  /* 
+  HttpRequest is intercepted and passed into authController
+  
+  Returns an Observable of HttpResponse
+ */
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return of(req).pipe(delay(delay), map(authController));
+  }
+}
